9ca2127c79656fea6703ac2dc64bdbd3acb1315c
Seungwon Park <yyyyy@snu.ac.kr>
2019-10-22 15:50:34 +0900

    swap G/D order for twoloader

--- utils/train.py
+++ utils/train.py
@@ -66,20 +66,6 @@ def train(args, pt_dir, chkpt_path, trainloader, valloader, writer, logger, hp, 
                 melD = melD.cuda()
                 audioD = audioD.cuda()
 
-                # generator
-                optim_g.zero_grad()
-                fake_audio = model_g(melG)[:, :, :hp.audio.segment_length]
-                disc_fake = model_d(fake_audio)
-                disc_real = model_d(audioG)
-                loss_g = 0.0
-                for (feats_fake, score_fake), (feats_real, _) in zip(disc_fake, disc_real):
-                    loss_g += torch.mean(torch.sum(torch.pow(score_fake - 1.0, 2), dim=[1, 2]))
-                    for feat_f, feat_r in zip(feats_fake, feats_real):
-                        loss_g += hp.model.feat_match * torch.mean(torch.abs(feat_f - feat_r))
-
-                loss_g.backward()
-                optim_g.step()
-
                 # discriminator
                 fake_audio = model_g(melD)[:, :, :hp.audio.segment_length]
                 fake_audio = fake_audio.detach()
@@ -96,6 +82,20 @@ def train(args, pt_dir, chkpt_path, trainloader, valloader, writer, logger, hp, 
                     loss_d.backward()
                     optim_d.step()
                     loss_d_sum += loss_d
+
+                # generator
+                optim_g.zero_grad()
+                fake_audio = model_g(melG)[:, :, :hp.audio.segment_length]
+                disc_fake = model_d(fake_audio)
+                disc_real = model_d(audioG)
+                loss_g = 0.0
+                for (feats_fake, score_fake), (feats_real, _) in zip(disc_fake, disc_real):
+                    loss_g += torch.mean(torch.sum(torch.pow(score_fake - 1.0, 2), dim=[1, 2]))
+                    for feat_f, feat_r in zip(feats_fake, feats_real):
+                        loss_g += hp.model.feat_match * torch.mean(torch.abs(feat_f - feat_r))
+
+                loss_g.backward()
+                optim_g.step()
 
                 step += 1
                 # logging
